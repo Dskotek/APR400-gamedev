32204830e452bb5f275cbd4d6d0d9d14caef4544
Andreas <tarkovbear@icloud.com>
2025-10-29 13:23:04 +0100

    lagt till scener och tagit bort bin/obj för att underlätta push pull

--- MonoGameLibrary/Core.cs
+++ MonoGameLibrary/Core.cs
@@ -5,6 +5,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using MonoGameLibrary.Input;
 using MonoGameLibrary.Audio;
+using MonoGameLibrary.Scenes;
 
 namespace MonoGameLibrary;
 
@@ -16,6 +17,12 @@ public class Core : Game
     /// Gets a reference to the Core instance.
     /// </summary>
     public static Core Instance => s_instance;
+
+        // The scene that is currently active.
+    private static Scene s_activeScene;
+
+    // The next scene to switch to, if there is one.
+    private static Scene s_nextScene;
 
     /// <summary>
     /// Gets the graphics device manager to control the presentation of graphics.
@@ -107,6 +114,13 @@ public class Core : Game
 
         Audio = new AudioController();
     }
+    protected override void UnloadContent()
+    {
+        // Dispose of the audio controller.
+        Audio.Dispose();
+
+        base.UnloadContent();
+    }
     protected override void Update(GameTime gameTime)
     {
         //Update the input manager
@@ -118,7 +132,66 @@ public class Core : Game
         {
             Exit();
         }
-        
+
+        // if there is a next scene waiting to be switch to, then transition
+        // to that scene.
+        if (s_nextScene != null)
+        {
+            TransitionScene();
+        }
+
+        // If there is an active scene, update it.
+        if (s_activeScene != null)
+        {
+            s_activeScene.Update(gameTime);
+        }
+
         base.Update(gameTime);
+    }
+    protected override void Draw(GameTime gameTime)
+    {
+        // If there is an active scene, draw it.
+        if (s_activeScene != null)
+        {
+            s_activeScene.Draw(gameTime);
+        }
+
+        base.Draw(gameTime);
+    }
+
+    public static void ChangeScene(Scene next)
+    {
+        // Only set the next scene value if it is not the same
+        // instance as the currently active scene.
+        if (s_activeScene != next)
+        {
+            s_nextScene = next;
+        }
+    }
+
+    private static void TransitionScene()
+    {
+        // If there is an active scene, dispose of it.
+        if (s_activeScene != null)
+        {
+            s_activeScene.Dispose();
+        }
+
+        // Force the garbage collector to collect to ensure memory is cleared.
+        GC.Collect();
+
+        // Change the currently active scene to the new scene.
+        s_activeScene = s_nextScene;
+
+        // Null out the next scene value so it does not trigger a change over and over.
+        s_nextScene = null;
+
+        // If the active scene now is not null, initialize it.
+        // Remember, just like with Game, the Initialize call also calls the
+        // Scene.LoadContent
+        if (s_activeScene != null)
+        {
+            s_activeScene.Initialize();
+        }
     }
 }
